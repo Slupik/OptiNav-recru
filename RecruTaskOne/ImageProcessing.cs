b3ca61403fc9ce976be99b967d9e7cc68d30b39f
Slupik <Slupik98@gmail.com>
2020-06-04 20:32:12 +0200

    refactor: Rename proceeding function

--- RecruTaskOne/ImageProcessing.cs
+++ RecruTaskOne/ImageProcessing.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RecruTaskOne
@@ -14,7 +10,7 @@ namespace RecruTaskOne
         void LoadImage(string path);
         void LoadImage(Image image);
 
-        void Process();
+        void ToMainColors();
 
         void SaveProcessed(string outputPath);
     }
@@ -44,19 +40,28 @@ namespace RecruTaskOne
             oryginal = image;
         }
 
-        public abstract void Process();
+        public abstract void ToMainColors();
 
-        protected Color ProcessPixel(Color pixel)
+        protected void ChangePixel(ref byte red, ref byte green, ref byte blue)
         {
-            if (pixel.R >= pixel.G && pixel.R >= pixel.B)
+            if (red >= green && red >= blue)
             {
-                return Color.FromArgb(pixel.A, 255, 0, 0);
+                red = (byte)255;
+                green = (byte)0;
+                blue = (byte)0;
             }
-            if (pixel.G >= pixel.R && pixel.G >= pixel.B)
+            else if (green >= red && green >= blue)
             {
-                return Color.FromArgb(pixel.A, 0, 255, 0);
+                red = (byte)0;
+                green = (byte)255;
+                blue = (byte)0;
             }
-            return Color.FromArgb(pixel.A, 0, 0, 255);
+            else
+            {
+                red = (byte)0;
+                green = (byte)0;
+                blue = (byte)255;
+            }
         }
 
         public void SaveProcessed(string outputPath)
@@ -72,29 +77,48 @@ namespace RecruTaskOne
     public class SynchronzousImageProcessing : ImageProcessing
     {
 
-        public override void Process()
+        public override void ToMainColors()
         {
             if (oryginal == null)
             {
                 throw new SourceImageNotFoundException();
             }
             Bitmap source = new Bitmap(oryginal);
-            for (int x = 0; x < source.Width; x++)
+
+            unsafe
             {
-                for (int y = 0; y < source.Height; y++)
+                BitmapData imageData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadWrite, source.PixelFormat);
+
+                int bytesPerPixel = Image.GetPixelFormatSize(source.PixelFormat) / 8;
+                int widthInBytes = imageData.Width * bytesPerPixel;
+                int heightInPixels = imageData.Height;
+
+                byte* startPointer = (byte*)imageData.Scan0;
+
+                for (int y = 0; y < heightInPixels; y++)
                 {
-                    Color sourceColor = source.GetPixel(x, y);
-                    source.SetPixel(x, y, ProcessPixel(sourceColor));
-                }
+                    byte* srcCurrentLine = startPointer + (y * imageData.Stride);
+                    for (int x = 0; x < widthInBytes; x += bytesPerPixel)
+                    {
+                        ChangePixel(
+                                    ref srcCurrentLine[x + 2],
+                                    ref srcCurrentLine[x + 1],
+                                    ref srcCurrentLine[x]
+                            );
+                    }
+                };
+                source.UnlockBits(imageData);
             }
+
             processed = source;
         }
+
     }
 
     public class AsynchronousImageProcessing : ImageProcessing
     {
 
-        public override void Process()
+        public override void ToMainColors()
         {
             if (oryginal == null)
             {
@@ -128,28 +152,6 @@ namespace RecruTaskOne
             }
 
             processed = source;
-        }
-
-        private void ChangePixel(ref byte red, ref byte green, ref byte blue)
-        {
-            if (red >= green && red >= blue)
-            {
-                red = (byte)255;
-                green = (byte)0;
-                blue = (byte)0;
-            }
-            else if (green >= red && green >= blue)
-            {
-                red = (byte)0;
-                green = (byte)255;
-                blue = (byte)0;
-            }
-            else
-            {
-                red = (byte)0;
-                green = (byte)0;
-                blue = (byte)255;
-            }
         }
 
     }
