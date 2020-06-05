49119cabd3a34cb12f12ee3e74c154006ae5c7d8
Slupik <Slupik98@gmail.com>
2020-06-05 20:05:14 +0200

    feat: Better handling async states of app

--- RecruTaskTwo/ViewModels/MainViewModel.cs
+++ RecruTaskTwo/ViewModels/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RecruTaskTwo.ViewModels
@@ -37,6 +38,41 @@ namespace RecruTaskTwo.ViewModels
             }
         }
 
+        private bool _stateInformationIsVisible = false;
+        public bool StateInformationIsVisible
+        {
+            get { return _stateInformationIsVisible; }
+            set
+            {
+                _stateInformationIsVisible = value;
+                NotifyOfPropertyChange(() => StateInformationIsVisible);
+            }
+        }
+
+        private bool _allowToInteract = true;
+        public bool AllowToInteract
+        {
+            get { return _allowToInteract; }
+            set
+            {
+                _allowToInteract = value;
+                if (value) StateInformationIsVisible = false;
+                NotifyOfPropertyChange(() => AllowToInteract);
+            }
+        }
+
+        private string _stateInformation;
+        public string StateInformation
+        {
+            get { return _stateInformation; }
+            set
+            {
+                _stateInformation = value;
+                StateInformationIsVisible = true;
+                NotifyOfPropertyChange(() => StateInformation);
+            }
+        }
+
         private string _imagePath;
         public string ImagePath
         {
@@ -59,28 +95,42 @@ namespace RecruTaskTwo.ViewModels
             }
         }
 
-        public void LoadImage()
+        public async void LoadImage()
         {
+            AllowToInteract = false;
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
                 ImagePath = openFileDialog.FileName;
-                SynchronousProcessor.LoadImage(openFileDialog.FileName);
-                ImageIsSelected = true;
+                await Task.Run(() =>
+                {
+                    StateInformation = "Wczytywanie pliku...";
+                    SynchronousProcessor.LoadImage(openFileDialog.FileName);
+                    ImageIsSelected = true;
+                    AllowToInteract = true;
+                });
             }
         }
 
-        public void ProcessImage()
+        public async void ProcessImage()
         {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            SynchronousProcessor.ToMainColors();
-            stopWatch.Stop();
+            await Task.Run(() =>
+            {
+                AllowToInteract = false;
+                StateInformation = "Przetwarzanie danych...";
 
-            TimeSpan ts = stopWatch.Elapsed;
-            ProcessingTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-            TimeInfoContainerIsVisible = true;
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                SynchronousProcessor.ToMainColors();
+                stopWatch.Stop();
+
+                TimeSpan ts = stopWatch.Elapsed;
+                ProcessingTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+                TimeInfoContainerIsVisible = true;
+                AllowToInteract = true;
+            });
         }
     }
 }
