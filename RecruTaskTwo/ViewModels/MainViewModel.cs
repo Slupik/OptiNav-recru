cbf4b04e33207ca3bc3c91b7b50dfe6aecee8908
Slupik <Slupik98@gmail.com>
2020-06-05 18:52:02 +0200

    feat: Create base for binding

--- RecruTaskTwo/ViewModels/MainViewModel.cs
+++ RecruTaskTwo/ViewModels/MainViewModel.cs
@@ -9,6 +9,56 @@ namespace RecruTaskTwo.ViewModels
 {
     public class MainViewModel : Screen
     {
+        private bool _timeInfoContainerIsVisible = false;
+        public bool TimeInfoContainerIsVisible
+        {
+            get { return _timeInfoContainerIsVisible; }
+            set
+            {
+                _timeInfoContainerIsVisible = value;
+                NotifyOfPropertyChange(() => TimeInfoContainerIsVisible);
+            }
+        }
 
+        private bool _imageIsSelected = false;
+        public bool ImageIsSelected
+        {
+            get { return _imageIsSelected; }
+            set
+            {
+                _imageIsSelected = value;
+                NotifyOfPropertyChange(() => ImageIsSelected);
+            }
+        }
+
+        private string _imagePath;
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set
+            {
+                _imagePath = value;
+                NotifyOfPropertyChange(() => ImagePath);
+            }
+        }
+
+        private string _processingTime;
+        public string ProcessingTime
+        {
+            get { return _processingTime; }
+            set
+            {
+                _processingTime = value;
+                NotifyOfPropertyChange(() => ProcessingTime);
+            }
+        }
+
+        public void LoadImage()
+        {
+            ProcessingTime = "30";
+            ImagePath = "img path";
+            TimeInfoContainerIsVisible = true;
+            ImageIsSelected = true;
+        }
     }
 }
